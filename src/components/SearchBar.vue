094d0a5324359d1eb8b2a1bae4b19e7356951215
DenuccioDiego <diegodenuccio95@gmail.com>
2021-11-18 13:07:36 +0100

    cycle to print the titles

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -2,6 +2,11 @@
      <div>
           <input type="text" placeholder="Search...">
           <button @click="callApi" type="button">Start</button>
+          
+          <div v-for="movie in movies" :key="movie.id">
+               {{movie.title}}
+          </div>
+
      </div>
 </template>
 
@@ -25,6 +30,8 @@ export default{
                .get("https://api.themoviedb.org/3/search/movie?api_key=30f94b70393e21156823d2636f7229b7&language=en-US&include_adult=false&query=ritorno")
                .then(response=>{
                     console.log(response.data.results)
+                    this.movies = response.data.results
+                    console.log(this.movies)
                })
 
                
