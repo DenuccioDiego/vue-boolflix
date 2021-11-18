be4563dbacb88b8f4d715ef1142aafd5b5701974
DenuccioDiego <diegodenuccio95@gmail.com>
2021-11-18 11:45:25 +0100

    Register and use base page components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
+
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <MainSection />
+    <Footer />  
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue"
+import MainSection from "./components/MainSection.vue"
+import Footer from "./components/Footer.vue"
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Header,
+    MainSection,
+    Footer,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
