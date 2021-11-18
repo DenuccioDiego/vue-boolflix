a20b424d4831032ba70f99c3dc58b83b119858df
DenuccioDiego <diegodenuccio95@gmail.com>
2021-11-18 13:02:32 +0100

    function declaration for call

--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -1,13 +1,17 @@
 <template>
      <div>
-
+          <SearchBar />
      </div>
 </template>
 
 <script>
+import SearchBar from './SearchBar.vue'
 
 
 export default{
+  components: { 
+       SearchBar 
+     },
     
 }
 </script>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,39 @@
+<template>
+     <div>
+          <input type="text" placeholder="Search...">
+          <button @click="callApi" type="button">Start</button>
+     </div>
+</template>
+
+<script>
+
+import axios from "axios"
+
+export default{
+    data(){
+         return{
+               movies : [],
+               error : "",
+          }
+         
+     },
+
+     methods:{
+
+          callApi(){
+               axios
+               .get("https://api.themoviedb.org/3/search/movie?api_key=30f94b70393e21156823d2636f7229b7&language=en-US&include_adult=false&query=ritorno")
+               .then(response=>{
+                    console.log(response.data.results)
+               })
+
+               
+          }
+
+     }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
